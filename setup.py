1051cb1ac5dd7012ffe5cf5f5c7174e7a7a0060e
Steven Sklar <steve@predata.com>
2017-07-21 14:49:42 -0400

    Published package

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="django-sendgrid-v4",
+    version="0.1.0a1",
+    description="An implementation of Django's EmailBackend compatible with sendgrid-python v4+",
+    long_description=long_description,
+    url="https://github.com/sklarsa/django-sendgrid-v4",
+    license="WTFPL",
+    author="Steven Sklar",
+    author_email="sklarsa@gmail.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+    ],
+    keywords="django email sendgrid backend",
+    packages=find_packages(exclude=[]),
+    install_requires=["django", "sendgrid"]
+)
