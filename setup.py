86cf1b7cea31a82be00042003794815baba67376
Steven Sklar <steve@predata.com>
2017-11-06 17:32:12 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name="django-sendgrid-v5",
-    version="0.6.81",
+    version="0.6.82",
     description="An implementation of Django's EmailBackend compatible with sendgrid-python v5+",
     long_description=long_description,
     url="https://github.com/sklarsa/django-sendgrid-v5",
